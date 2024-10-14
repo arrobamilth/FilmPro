f51d7efe4bb6c6c6f8d93174e909f1c8bff7dbee
Brayan980 <brayan.contreras@cecar.edu.co>
2024-10-14 18:23:54 -0500

    Pagina 3 y boton de ciencia ficcion agregados

--- FilmPro/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/FilmPro.AssemblyInfo.cs
+++ FilmPro/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/FilmPro.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FilmPro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+472c04f5a1e0690957d1eedb93b4f7f64a5b3160")]
 [assembly: System.Reflection.AssemblyProductAttribute("FilmPro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FilmPro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
